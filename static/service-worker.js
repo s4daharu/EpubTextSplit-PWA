f05d58930c880b59d0d557d81ea0acd4648672db
s4daharu <>
2025-03-20 15:29:13 +0530

    Last Sync: 2025-03-20 09:59:13 (Mobile)

---
+++ static/service-worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'text-splitter-v2';
+const STATIC_ASSETS = [
+  '/',
+  '/static/manifest.json',
+  '/static/icons/icon-192x192.png',
+  '/static/icons/icon-512x512.png',
+  '/app'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(STATIC_ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
